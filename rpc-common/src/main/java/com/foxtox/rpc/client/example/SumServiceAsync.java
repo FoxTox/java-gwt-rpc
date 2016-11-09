1f8e4f22c3619e1f47cc9b41a9bbd9fad60bc3cc
Alisa Patotskaya <alisa.moc.liamg@gmail.com>
2016-11-10 00:37:47 +0100

    Factor out the code that should be generated by GWT generator.

--- rpc-common/src/main/java/com/foxtox/rpc/client/example/SumServiceAsync.java
+++ rpc-common/src/main/java/com/foxtox/rpc/client/example/SumServiceAsync.java
@@ -4,5 +4,5 @@ import com.foxtox.rpc.client.AsyncCallback;
 
 public interface SumServiceAsync {
 	void getSum(Integer first, Integer second, AsyncCallback<Integer> callback);
-	Double getSum(Double first, Double second, AsyncCallback<Double> callback);
+	void getSum(Double first, Double second, AsyncCallback<Double> callback);
 }
--- rpc-server/src/main/java/com/foxtox/rpc/client/example/GwtEntryPoint.java
+++ rpc-server/src/main/java/com/foxtox/rpc/client/example/GwtEntryPoint.java
@@ -1,38 +1,22 @@
 package com.foxtox.rpc.client.example;
 
-import com.foxtox.rpc.client.gwt.JsonRequestSerializer;
-import com.foxtox.rpc.common.RequestSerializer;
+import com.foxtox.rpc.client.AsyncCallback;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.http.client.*;
 import com.google.gwt.user.client.Window;
 
 public class GwtEntryPoint implements EntryPoint {
 
 	public void onModuleLoad() {
-		System.out.println("Hey, I'm running!");
-		Window.alert("Hello, AJAX");
-		RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, "/sum");
-		
-		// TODO: Generate this with a generator.
-		RequestSerializer serializer = new JsonRequestSerializer();
-		serializer.setServiceName("com.foxtox.rpc.client.example.SumService");
-		serializer.setServiceMethod("getSum");
-		serializer.addParameter(10);
-		serializer.addParameter(20);
-		builder.setRequestData(new String(serializer.serialize()));
-		
-		builder.setCallback(new RequestCallback() {
-			public void onResponseReceived(Request request, Response response) {
-				Window.alert(response.getText());
-			}
+		final int first = 10, second = 20;
 
-			public void onError(Request request, Throwable throwable) {
+		SumServiceAsync sumService = new SumServiceAsyncImpl();
+		sumService.getSum(first, second, new AsyncCallback<Integer>() {
+			public void onSuccess(Integer returnValue) {
+				Window.alert(first + " + " + second + " == " + returnValue);
+			}
+			public void onFailure(String reason) {
+				Window.alert("Failed to compute " + first + " + " + second + ". Reason:\n" + reason);
 			}
 		});
-		try {
-			builder.send();
-		} catch (RequestException e) {
-			// ignore
-		}
 	}
 }
